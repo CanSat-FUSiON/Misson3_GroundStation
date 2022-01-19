7f6341beffedb974e11a80955541418f3e9e0b77
Hiroaki Honda <82698421+Damin3927@users.noreply.github.com>
2022-01-20 02:58:48 +0900

    add admin view

--- backend/image_process/admin.py
+++ backend/image_process/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import ImageTestModel
+
+
+@admin.register(ImageTestModel)
+class ImageTestModelAdmin(admin.ModelAdmin):
+    pass
