88f6014a4fad06d3f3ffe202b3f4c3dbba23c7fc
Hiroaki Honda <82698421+Damin3927@users.noreply.github.com>
2022-01-20 03:04:23 +0900

    change admin endpoint

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("fusion/admin/", admin.site.urls),
     path("fusion/", include("image_process.urls")),
 ]
