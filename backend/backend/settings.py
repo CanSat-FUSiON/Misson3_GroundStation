79326966b56716e15a0fcb658c81fb019a8dac91
Hiroaki Honda <82698421+Damin3927@users.noreply.github.com>
2022-01-21 18:30:07 +0900

    add postgre db

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,19 +11,26 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import environ
+
+env = environ.Env(
+    DEBUG=(bool, False),
+)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+env.read_env(BASE_DIR / ".env")
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-up1807%^k9ypqknsqp2l2h&ctho$a$c*)090k6(c1c*_d37)c!"
+# SECRET_KEY = "django-insecure-up1807%^k9ypqknsqp2l2h&ctho$a$c*)090k6(c1c*_d37)c!"
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env("DEBUG")
 
 ALLOWED_HOSTS = [
     "172.31.43.202",
@@ -89,9 +96,17 @@ WSGI_APPLICATION = "backend.wsgi.application"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
+    # "default": {
+    #     "ENGINE": "django.db.backends.sqlite3",
+    #     "NAME": BASE_DIR / "db.sqlite3",
+    # }
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "efuthp",
+        "USER": env("POSTGRESQL_USER"),
+        "PASSWORD": env("POSTGRESQL_PASSWORD"),
+        "HOST": env("POSTGRESQL_HOST"),
+        "PORT": "5432",
     }
 }
 
