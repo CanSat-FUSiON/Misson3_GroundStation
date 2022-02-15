fd7496027eab47dff1471a05dfdfcbd5b9c03f51
KazumaKato <kkazu021108@gmail.com>
2022-02-15 14:14:50 +0900

    no change

--- backend/control/urls.py
+++ backend/control/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 
 urlpatterns = [
-    path("right/", views.rightAPIView.as_view(), name="right"),
-    path("left/", views.leftAPIView.as_view(), name="left"),
-    path("forward/", views.forwardAPIView.as_view(), name="forward"),
-    path("back/", views.backAPIView.as_view(), name="back"),
+    path("right/", views.rightAPIView.as_view(), name="right"),  # 右回り
+    path("left/", views.leftAPIView.as_view(), name="left"),  # 左回り
+    path("forward/", views.forwardAPIView.as_view(), name="forward"),  # 前進
+    path("back/", views.backAPIView.as_view(), name="back"),  # 後退
 ]
