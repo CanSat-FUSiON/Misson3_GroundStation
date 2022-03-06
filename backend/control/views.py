eb21e70d3ae903378645ca86beb7198de29f5d15
KazumaKato <kkazu021108@gmail.com>
2022-03-06 11:33:00 +0900

    no GPS

--- backend/control/views.py
+++ backend/control/views.py
@@ -5,12 +5,21 @@ from rest_framework.response import Response
 from rest_framework import status
 import requests
 
-IP_address_wroom = 'http://192.168.3.15'
 
+IP_address_wroom = 'http://192.168.3.15'
 
 class rightAPIView(APIView):
     def get(self, request: Request, time) -> Response:
         r = requests.get(IP_address_wroom + '/right' + '?ms=' + str(time))
+        if r.status_code==200:
+            return Response()
+        else:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        
+
+class right0APIView(APIView):
+    def get(self, request: Request) -> Response:
+        r = requests.get(IP_address_wroom + '/right')
         if r.status_code==200:
             return Response()
         else:
@@ -26,9 +35,27 @@ class leftAPIView(APIView):
             return Response(status=status.HTTP_404_NOT_FOUND)
 
 
+class left0APIView(APIView):
+    def get(self, request: Request) -> Response:
+        r = requests.get(IP_address_wroom + '/left') 
+        if r.status_code==200:
+            return Response()
+        else:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+
 class forwardAPIView(APIView):
     def get(self, request: Request, time) -> Response:
         r = requests.get(IP_address_wroom + '/forward' + '?ms=' + str(time))
+        if r.status_code==200:
+            return Response()
+        else:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        
+
+class forward0APIView(APIView):
+    def get(self, request: Request) -> Response:
+        r = requests.get(IP_address_wroom + '/forward')
         if r.status_code==200:
             return Response()
         else:
@@ -42,15 +69,33 @@ class backAPIView(APIView):
             return Response()
         else:
             return Response(status=status.HTTP_404_NOT_FOUND)
-
-
-class gpsAPIView(APIView):
+        
+        
+class back0APIView(APIView):
     def get(self, request: Request) -> Response:
-        r = requests.get(IP_address_wroom + '/GPS')
+        r = requests.get(IP_address_wroom + '/back')
         if r.status_code==200:
             return Response()
         else:
             return Response(status=status.HTTP_404_NOT_FOUND)
+
+
+# class gpsAPIView(APIView):
+#     def get(self, request: Request, time) -> Response:
+#         r = requests.get(IP_address_wroom + '/GPS' + '?ver=' + str(time))
+#         if r.status_code==200:
+#             return Response()
+#         else:
+#             return Response(status=status.HTTP_404_NOT_FOUND)
+        
+
+# class gps0APIView(APIView):
+#     def get(self, request: Request) -> Response:
+#         r = requests.get(IP_address_wroom + '/GPS')
+#         if r.status_code==200:
+#             return Response()
+#         else:
+#             return Response(status=status.HTTP_404_NOT_FOUND)
 
 
 class getdataAPIView(APIView):
