4afe0f7602b9bc41b6ff97f6c256405cd0c7e4e3
tkt <tkt151221@gmail.com>
2023-02-20 21:02:41 +0900

    revisin backend url

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/v1/image/", include("image_process.urls")),
-    path("ap1/v1/control/", include("control.urls")),
+    path("api/v1/control/", include("control.urls")),
 ]
